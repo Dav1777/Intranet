088ed651161bd3eb19cbe0018f47d63e726518f4
Davi <davivalerianosilva@gmail.com>
2025-10-17 20:09:46 -0300

    Update Funcionario.java
    funcionario

--- intranet/src/main/java/com/saf/intranet/models/Funcionario.java
+++ intranet/src/main/java/com/saf/intranet/models/Funcionario.java
@@ -1,4 +1,30 @@
 package com.saf.intranet.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Funcionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String matricula;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String cargo;
+    private Setor setor;
+    private String senha;
+    @Embedded
+    private Endereco endereco;
+    private Long telefone;
+
+
+
 }
